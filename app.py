6b1bb83bd70c4e8dc2d93bb37a7e9697e658c8d6
Jedrzej Ciechanowski <jk.ciechanowski@gmail.com>
2024-12-10 20:37:34 +0100

    deleted useless comments

--- app.py
+++ app.py
@@ -26,16 +26,3 @@ def get_attenuation():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=4200)
-
-# start_lat, start_lon = 49.809596,19.052732
-# end_lat, end_lon = 49.699071,19.269432
-
-# steps = generate_steps(start_lat, start_lon,end_lat, end_lon)
-# distance_height=get_elevation(steps)
-
-# peaks = get_ep_peak(distance_height, {})
-# print("Peaks needed in epstein-peterson method:",peaks)
-
-# attenuation, params = calculate_attenuation(distance_height,peaks,450)
-# print("Attenuation:",attenuation)
-# print(params)
